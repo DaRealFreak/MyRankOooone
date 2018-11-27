5c870fd90d2eb42e2b76640e45398e9cebcf96c6
Steffen Keuper <steffen.keuper@web.de>
2018-11-27 16:01:24 +0100

    [BUGFIX] fix import of slugify functions

--- my_rank_oooone/ddnet/__init__.py
+++ my_rank_oooone/ddnet/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/local/bin/python
 # coding: utf-8
-from my_rank_oooone.ddnet.urls import slugify2, unslugify
+from my_rank_oooone.ddnet.urls import slugify, deslugify
 
-__all__ = [slugify2, unslugify]
+__all__ = [slugify, deslugify]
