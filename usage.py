0e094a5aa23a0665e4ed47678c027eeb723d66b4
Steffen Keuper <steffen.keuper@web.de>
2018-11-27 15:34:30 +0100

    [TASK] update usage for main file

--- usage.py
+++ usage.py
@@ -3,8 +3,8 @@
 #
 # This is an example usage
 
-from my_rank_oooone.databaseIO import DatabaseIO
+from my_rank_oooone import MyRankOooone
 
 if __name__ == '__main__':
-    db = DatabaseIO()
-    db.add_player('meter\'.')
+    rank = MyRankOooone()
+    rank.add_player('meter\'.')
